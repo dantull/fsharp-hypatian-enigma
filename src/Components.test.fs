b2fd4425dfaaa816b5bda4db6b0421740a1e5316
Dan Tull <dantull@gmail.com>
2026-05-17 23:51:29 -0500

    Fix stale test.

--- src/Components.test.fs
+++ src/Components.test.fs
@@ -4,24 +4,16 @@ open Vitest
 open Feliz
 
 Vitest.describe (
-    "Counter",
+    "HexGrid",
     fun () ->
         Vitest.test (
-            "increments the counter when button is clicked",
+            "renders the hex grid svg",
             fun () -> promise {
+                let result = RTL.render (App.Components.HexGrid())
+                let container = result.container
+                let svg = container.querySelector ("svg")
 
-                let ele = RTL.render (App.Components.Counter())
-
-                let text = ele.getByTestId ("counter-display")
-
-                Vitest.expect(text).toBeInTheDocument ()
-                Vitest.expect(text).toHaveTextContent ("0")
-
-                let btn = ele.getByTestId ("inc-btn")
-
-                do! UserEvent.userEvent.click (btn)
-
-                Vitest.expect(text).toHaveTextContent ("1")
+                Vitest.expect(svg).toBeTruthy ()
             }
         )
 )
